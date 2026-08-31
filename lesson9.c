c7193a435887c7a1cbb7cae11b4ad5361bb3c60a
yousifsalih19 <ysalih0419@gmail.com>
2026-08-30 20:04:06 -0500

    start 9

---
+++ lesson9.c
@@ -0,0 +1,12 @@
+/*
+Take two command line arguments from the user, their first and last name, and then print a welcome message for the user.
+
+The program should check for two arguments and then print the program's usage instructions if the user submits too few or too many arguments.
+
+ */
+
+#include <stdio.h>
+
+int main(){
+	return 0;
+}
